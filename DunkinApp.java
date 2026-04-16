fe8c2c3681fd2953e44bad54995059d3ba30bb13
Akash Roy <akashavro4@gmail.com>
2026-04-16 08:49:43 -0700

    Add files via upload

---
+++ DunkinApp.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+interface TimeTracker {
+    void showDashboard();
+}
+
+abstract class DunkinEmployee implements TimeTracker {
+    private String name;
+    private String role;
+    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("hh:mm a");
+
+    public DunkinEmployee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    @Override
+    public void showDashboard() {
+        LocalTime clockIn = LocalTime.of(17, 0);          
+        LocalTime breakStart = LocalTime.of(20, 30);      
+        LocalTime breakEnd = LocalTime.of(21, 0);         
+        LocalTime clockOut = LocalTime.of(22, 0);         
+
+        System.out.println("==========================================");
+        System.out.println("   DUNKIN' STAFF DASHBOARD - " + name.toUpperCase());
+        System.out.println("==========================================");
+        System.out.println(" ROLE          : " + role);
+        System.out.println(" STATUS        : SHIFT COMPLETED");
+        System.out.println("------------------------------------------");
+        System.out.println(" [START] Clocked In    @ " + clockIn.format(dtf));
+        System.out.println(" [BREAK] Started       @ " + breakStart.format(dtf));
+        System.out.println(" [BREAK] Finished      @ " + breakEnd.format(dtf));
+        System.out.println(" [END]   Clocked Out   @ " + clockOut.format(dtf));
+        System.out.println("------------------------------------------");
+        System.out.println(" TOTAL WORK    : 5 Hours");
+        System.out.println(" TOTAL BREAK   : 30 Minutes");
+        performDuty();
+        System.out.println("==========================================\n");
+    }
+
+    public abstract void performDuty();
+    public String getName() { return name; }
+}
+
+class CrewMember extends DunkinEmployee {
+    public CrewMember(String name) { super(name, "Crew Member"); }
+    @Override
+    public void performDuty() {
+        System.out.println(" WORK LOG      : Prepped donuts & served coffee.");
+    }
+}
+
+class Manager extends DunkinEmployee {
+    public Manager(String name) { super(name, "Store Manager"); }
+    @Override
+    public void performDuty() {
+        System.out.println(" WORK LOG      : Managed staff & closed registers.");
+    }
+}
+
+class DunkinApp {
+    public static void main(String[] args) {
+        List<DunkinEmployee> staff = new ArrayList<>();
+        
+        staff.add(new CrewMember("Akash"));
+        staff.add(new Manager("Siya"));
+
+        for (DunkinEmployee emp : staff) {
+            emp.showDashboard();
+        }
+    }
+}
